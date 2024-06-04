822b070ba6456d4cd6985b214bde1709f61ccc86
B4BF00LN4MU <boy566@naver.com>
2024-06-04 17:52:53 +0900

    20240604

--- main.cpp
+++ main.cpp
@@ -3,24 +3,35 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
-	FILE* fp = fopen("Map.txt", "r");
-	char c = 0;
-	char line[80];
+	string Content;
+
+	ifstream InputFile;
 	
-	//EOF가 뭔데 End Of File? 파일의 끝이냐 라는데?
-	
-	while (fgets(line, 80, fp) != nullptr)
+	InputFile.open("data.map");
+
+	while (InputFile.getline(Content, 80))
 	{
-		cout << line;
+		cout << Content << endl;
 	}
+
+	InputFile.close();
+
+	//FILE* fp = fopen("Map.txt", "r");
+	//char c = 0;
+	//char line[80];
+	//
+	////EOF가 뭔데 End Of File? 파일의 끝이냐 라는데?
+	//
+	//while (fgets(line, 80, fp) != nullptr)
+	//{
+	//	cout << line;
+	//}
 	
-
-	fgetc(fp);
-
 	return 0;
 }
